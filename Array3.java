8beb91181a7753e2a51c05d847c616348ae870c5
Sneha Nandvadekar <jnsneha04@gmail.com>
2025-11-12 16:17:42 +0530

    Array string

---
+++ Array3.java
@@ -0,0 +1,34 @@
+package core;
+
+import java.util.Arrays;
+
+public class Array3 {
+
+	public static void main(String args[])
+	{
+		String name = "I am Sneha";
+		boolean ans = name.contains("Sn");
+		System.out.println(ans);
+		
+		boolean ans1 = name.contains("sn");
+		System.out.println(ans1);
+		
+		boolean ends = name.endsWith("eha");
+		System.out.println(ends);
+		
+		boolean ends1= name.endsWith(" eha");
+		System.out.println(ends1);
+		
+		char name1[] = name.toCharArray();
+		System.out.println(Arrays.toString(name1));
+		
+		String s1 = name.toUpperCase();
+		System.out.println(s1);
+		
+		String s2 = name.toLowerCase();
+		System.out.println(s2);
+		
+		int i = s2.length();
+		System.out.println("Length of string is "+i);
+	}
+}
